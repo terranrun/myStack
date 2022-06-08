48b104b1a574ce507d488272935f9c619cb862fd
Mikhail VV <esto045@gmail.com>
2022-06-08 17:36:22 +0300

    доп3

--- MyStack/Program.cs
+++ MyStack/Program.cs
@@ -1,20 +1,20 @@
-﻿//var s = new Stack("a", "b", "c");
-//// size = 3, Top = 'c'
-//Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
-//var deleted = s.Pop(); 
-//// Извлек верхний элемент 'c' Size = 2
-//Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
-//s.Add("d");
-//// size = 3, Top = 'd'
-//Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
-//s.Pop();
-//Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
-//s.Pop();
-//Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
-//s.Pop();
-//// size = 0, Top = null
-//Console.WriteLine($"size = {s.Size}, Top = {(s.top == null ? "null" : s.top)}");
-//s.Pop();
+﻿var s = new Stack("a", "b", "c");
+// size = 3, Top = 'c'
+Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
+var deleted = s.Pop();
+// Извлек верхний элемент 'c' Size = 2
+Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+s.Add("d");
+// size = 3, Top = 'd'
+Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
+s.Pop();
+Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
+s.Pop();
+Console.WriteLine($"size = {s.Size}, Top = '{s.top}'");
+s.Pop();
+// size = 0, Top = null
+Console.WriteLine($"size = {s.Size}, Top = {(s.top == null ? "null" : s.top)}");
+s.Pop();
 
 //var s = new Stack("a", "b", "c");
 //s.Merge(new Stack("1", "2", "3"));
@@ -22,8 +22,8 @@
 //Console.ReadKey();
 
 
-var s = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"));
-Console.ReadKey();
+//var s = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"));
+//Console.ReadKey();
 
 
 
@@ -39,13 +39,58 @@ static class StackExtension
             item = stack1.Pop();
             stack.Add(item);
         }
-        
+
     }
 }
 
+public class StackItem
+{
+    private string[]? _items;
+    public int Count;
+
+    public void AddRange(string[] items)
+    {
+        this._items = items;
+        Count = _items.Length;
+    }
+
+    public void Add(string item)
+    {
+        if(_items.Length == Count)
+        {
+            string[] largerArray = new string[Count * 2];
+            Array.Copy(_items, largerArray, Count);
+
+            _items = largerArray;
+        }
+        _items[Count++] = item;
+    }
+
+    public void Remove()
+    {
+        if(Count==0)
+        {
+            throw new InvalidOperationException();
+        }
+        _items[--Count] = null;
+
+    }
+
+    public string Peek()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        return _items[Count - 1];
+    }
+
+}
+
+
 public class Stack
 {
-    private List<string> stock = new List<string>();
+    private StackItem stackItem = new StackItem();
 
     public int Size { get; private set; }
 
@@ -72,52 +117,39 @@ public class Stack
         }
         return ListItem;
     }
+  
 
     public Stack(params string[] stocks)
     {
-
-        stock.AddRange(stocks);
-        Size = stock.Count();
-        top = stock[Size - 1];
+        stackItem.AddRange(stocks);
+        Size = stackItem.Count;
+        top = stackItem.Peek();
     }
 
     public void Add(string item)
     {
-
-        stock.Add(item);
-        Size = stock.Count;
-        top = stock[Size - 1];
-
+        stackItem.Add(item);
+        Size = stackItem.Count;
+        top = stackItem.Peek();
     }
 
     public string Pop()
     {
-
         if (Size == 0)
         {
             throw new InvalidOperationException("Стек пустой");
         }
-        result = stock[Size - 1];
-        stock.Remove(result);
-        Size = stock.Count;
+        result = stackItem.Peek();
+        stackItem.Remove();
+        Size = stackItem.Count;
         if (Size == 0)
         {
             top = null; 
         }
         else 
         { 
-            top = stock[Size - 1]; 
+            top = stackItem.Peek(); 
         }
         return result;
-
-
     }
-
-
-    public string Peek()
-    {
-        top = stock[Size - 1];
-        return top;
-    }
-
 }
